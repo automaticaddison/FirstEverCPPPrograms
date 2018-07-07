5abd66dc8dcfdcc2b98c238f77d4b00ce34a3874
automaticaddison <38732101+automaticaddison@users.noreply.github.com>
2018-07-07 12:04:24 -0700

    Minor formatting changes

--- compute_average.cc
+++ compute_average.cc
@@ -1,4 +1,4 @@
-/*******************************************************************************
+/******************************************************************************
  *  @file           compute_average.cc
  *  @author         Addison Sears-Collins
  *  @date           7/7/2018  
@@ -9,7 +9,7 @@
  *  Purpose: This is a program reads three separate numbers entered by the
  *           user. It then calculates the average of these three numbers 
  *           and displays the average to the console.
- ******************************************************************************/
+ *****************************************************************************/
 // Include iostream library to support the console input and output
 #include <iostream> 
 
@@ -19,18 +19,18 @@ using namespace std;
 // The main method that drives the program
 int main() {  
 
-	// Prompt the user to enter three numbers
-	double number1, number2, number3;
-	cout << "Enter three numbers separated by spaces: " << endl;
-	cin >> number1 >> number2 >> number3;
-	
-	// Calculate the average
-	double average = (number1 + number2 + number3) / 3;
+  // Prompt the user to enter three numbers
+  double number1, number2, number3;
+  cout << "Enter three numbers separated by spaces: " << endl;
+  cin >> number1 >> number2 >> number3;
 
-	// Display the average to the console
-	cout << "The average of " << number1 << ", " << number2 << ", and " 
-		<< number3 << " is " << average << endl;
-		
-	// Exit the program
-	return 0;
+  // Calculate the average
+  double average = (number1 + number2 + number3) / 3;
+
+  // Display the average to the console
+  cout << "The average of " << number1 << ", " << number2 << ", and " 
+    << number3 << " is " << average << endl;
+
+  // Exit the program
+  return 0;
 }
